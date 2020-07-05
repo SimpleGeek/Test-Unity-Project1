d9b51a3c408d92872329467f04a7e8b9e1879f70
SimpleGeek <39005277+SimpleGeek@users.noreply.github.com>
2020-07-05 17:30:38 -0400

    Added new building + door animation

---
+++ Assets/Scripts/GeneralScripts/OpenDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private Animator doorAnim;
+    
+    void Start()
+    {
+        doorAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+	{
+        if (other.tag == "Player")
+        {
+            doorAnim.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            doorAnim.SetBool("Open", false);
+        }
+    }
+}
